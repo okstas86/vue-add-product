667ddf237005b2f04fb84ea1cf421b1142579a0a
okstas86 <stanislavocunev1@gmail.com>
2023-09-22 18:39:49 +0200

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # add-product
 
+## A Vue-based application for adding products, built using the Composition API
+
+<img align="left" alt="screen1" width="600"  src="./public/all-product.png" />
+<img align="left" alt="screen2" width="600" src="./public/add-product.png" />
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, provide } from 'vue';
+
+import TheHeader from './components/TheHeader.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+  },
+  setup() {
+    const products = ref([
+      {
+        id: 'p1',
+        title: 'A Carpet',
+        description: 'A nice looking, maybe a little bit used carpet.',
+        price: 15.99,
+      },
+      {
+        id: 'p2',
+        title: 'A Book',
+        description: 'You can read it. Maybe you should read it.',
+        price: 12.99,
+      },
+    ]);
+
+    function addProduct(productData) {
+      const newProduct = {
+        id: new Date().toISOString(),
+        title: productData.title,
+        description: productData.description,
+        price: productData.price,
+      };
+      products.value.push(newProduct);
+    }
+
+    provide('products', products);
+    provide('addProduct', addProduct);
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
